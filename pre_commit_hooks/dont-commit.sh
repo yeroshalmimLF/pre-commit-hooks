18f88ba9fcd4a67a59b614edbc97539723abdfb8
YeroshalmimLF <Michael.Y@LegalFit.com>
2022-09-16 18:38:07 -0500

    updating script

--- pre_commit_hooks/dont-commit.sh
+++ pre_commit_hooks/dont-commit.sh
@@ -1,1 +1,17 @@
-echo "Hello"
+#!/usr/bin/env bash
+
+#
+# To prevent debug code from being accidentally committed, simply add a comment near your
+# debug code containing the keyword !nocommit and this script will abort the commit.
+#
+if git commit -v --dry-run | grep --color -B 2 -A 2 'DONTCOMMIT'  #>/dev/null 2>&1
+then
+  RED='\033[0;31m'
+  BLUE='\033[0;34m'
+  NC='\033[0m' # No Color
+  echo -e "${RED}ERROR${NC}"
+  echo "Trying to commit non-committable code."
+  echo -e "Remove the ${BLUE}DONTCOMMIT${NC} string and try again."
+  exit 1
+fi
+exit 0
